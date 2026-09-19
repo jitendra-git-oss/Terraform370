0aa744fd9e33d8f0c4315dc71747ca9a1f2fc820
jitendra-git-oss <jitendra.gupta78600@gmail.com>
2026-09-19 21:32:34 +0530

    updates

--- days-5-statefile-locking/main.tf
+++ days-5-statefile-locking/main.tf
@@ -15,3 +15,16 @@ resource "aws_instance" "name" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.dev.id
 }
+
+resource "aws_security_group" "name"  {
+  name        = "my-security-group"
+  description = "Allow ssh and HTTP traffic"
+  vpc_id      = aws_vpc.name.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
