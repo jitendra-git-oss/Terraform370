6efbf931f620e8338cc6dd161d5b576bb26da24a
jitendra-git-oss <jitendra.gupta78600@gmail.com>
2026-09-19 21:09:33 +0530

    updates

--- days-5-statefile-locking/main.tf
+++ days-5-statefile-locking/main.tf
@@ -9,3 +9,9 @@ resource "aws_subnet" "dev" {
   cidr_block = "10.0.0.0/24"
   vpc_id = aws_vpc.name.id
 }
+
+resource "aws_instance" "name" {
+  ami           = "ami-098f18a6382fb4b2d"
+  instance_type = "t3.micro"
+  subnet_id     = aws_subnet.dev.id
+}
